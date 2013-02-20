0ccabf6bec17da8fcc6c2b55a05b2efae719326f
Shay Weiss <shay.weiss@algowiz.com>
2013-02-20 14:57:14 +0200

    added trivial test

---
+++ test_foo.py
@@ -0,0 +1,10 @@
+import unittest
+import foo
+
+class TestFoo(unittest.TestCase):
+    def test_foo(self):
+        self.assertEqual(1, foo.foo())
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_bar_to_tick']
+    unittest.main()        
